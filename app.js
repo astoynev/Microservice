28fde8da6599717ca1923cb8db2c4918336ede7f
loged45 <98425135+loged45@users.noreply.github.com>
2022-04-13 22:02:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -63,56 +63,41 @@ function fetchProductList() {
 }
 
 function fetchOneProduct($id) {
-    jsonObj = [];
-    item = {};
     var product;
-
-    !($.trim($('#title').val()) == '') ? item ["title"] = $('#title').val(): '';
-    !($.trim($('#operating_system').val()) == '') ? item ["operating_system"] = $('#operating_system').val(): '';
-    !($.trim($('#min_price').val()) == '') ? item ["price_from"] = $('#min_price').val(): '';
-    !($.trim($('#max_price').val()) == '') ? item ["price_to"] = $('#max_price').val(): '';
-
-    jsonObj.push(item);
-
     $.ajax({
         url: Url+'GetOneProduct',
-        type: 'GET',
+        type: 'get',
         dataType: 'json',
+        data: {"product_id": $id},
         contentType: 'text/plain',
-        data: jsonObj[0],
-
         success: function (data) {
-
-            product = '<div class="col-sm-6 col-md-4 col-lg-3 mt-4" id="product'+item['id']+'">\n' +
-                    '            <div class="card card-inverse card-info">\n' +
-                    '                <img class="card-img-top" src="'+item['image']+'">\n' +
-                    '                <div class="card-block">\n' +
-                    '                    <h4><span class="badge badge-danger">'+item['price']+'</span></h4>\n' +
-                    '                    <div class="meta card-text">\n' +
-                    '                        <a style="color: deepskyblue">Category - Cell Phones</a>\n' +
-                    '                    </div>\n' +
-                    '                    <div class="card-text">\n' +
-                    '                        '+item['title']+'\n'
-                    '                    </div>\n' +
-                    '                </div>\n' +
-                    '                <div class="card-footer">\n' +
-                    '                    <small>More information ...</small>\n' +
-                    '                    <button class="btn btn-info float-right btn-sm" onclick="fetchOneProduct('+item['id']+')">Detail</button>\n' +
-                    '                </div>\n' +
-                    '                <div class="card-footer">\n' +
-                    '                    <button class="btn btn-info float-right btn-sm" onclick="addToCart('+item['id']+')">Add to Cart</button>\n' +
-                    '                </div>\n' +
-                    '            </div>\n' +
-                    '        </div>';
-
+            product = '';
+            product =  '<div>'+
+                        '<div class="card card-inverse card-info" style="width:900px; height:auto">'+
+                            '<img class="card-img-top" src="'+data['data']['List'][0]['image']+'" style="width:300px">'+
+                            '<h2>'+data['data']['List'][0]['title'].substring(0,15)+'</h2>'+
+                            '<img class="card-img-top" src="'+data['data']['List'][0]['image']+'" style="width:300px">'+
+                            '<div>'+data['data']['List'][0]['title']+'</div>'+
+                            '<div class="rating">\n' +
+                                '<button class="btn btn-info" id="comment" onclick="fetchComments('+data['data']['List'][0]['id']+')">'+data['data']['List'][0]['comment_count']+' comments</button>\n' +
+                            '</div>\n' +
+                            '<div class="card-block">' +
+                                '<h4><span class="badge">CURRENT PRICE: '+data['data']['List'][0]['price']+'</span></h4>'+
+                                '<h4><span class="badge">SCREEN SIZE: '+data['data']['List'][0]['screen_size']+'</span></h4>'+
+                                '<h4><span class="badge">WEIGHT: '+data['data']['List'][0]['item_weight']+'</span></h4>'+
+                                '<h4><span class="badge">CAMERA: '+data['data']['List'][0]['camera_description']+'</span></h4>'+
+                                '<h4><span class="badge">OPERATING SYSTEM: '+data['data']['List'][0]['operating_system']+'</span></h4>'+
+                            '</div>'+  
+                            '<div class="rating">\n' +
+                                '<button class="btn btn-info" id="setComment" data-toggle="modal" data-target="#exampleModal" data-whatever='+data['data']['List'][0]['id']+'>Submit Comment</button>\n' +
+                            '</div>\n' +                                                     
+                        '</div>'+
+                        '</div>'
+            
+                        
             $('#items').html(product);
-
-        },
-        error: function (data) {
-            alert("Error while fetching data.");
         }
-
-    });
+    })
 }
 
 function fetchComments($id) {
